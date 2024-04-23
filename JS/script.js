45c4e4cbd85d5ec091ea7c2ecab6a29aee36166e
gs-sreddygari <43570374+gs-sreddygari@users.noreply.github.com>
2024-04-23 21:00:38 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -50,11 +50,11 @@ function reIdentifyuser(){
 });*/
 
 //This is the original
-  /*(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-POK8MKKQ63Z3-2");*/
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-POK8MKKQ63Z3-2");
 
   //,{"engagementChecksumFileUrl": "http://px-support.epizy.com/JS/Engagement_checksum.json"}
 
