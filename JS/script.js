51ce0de4576708b3ccb1cd8f7ff5bc3c20e6701f
gs-sreddygari <43570374+gs-sreddygari@users.noreply.github.com>
2025-04-24 21:13:01 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -55,7 +55,7 @@ function reIdentifyuser(){
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-POK8MKKQ63Z3-2");
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-POK8MKKQ63Z3-2",{"localStorageCookie" : true});
 
   //,{"engagementChecksumFileUrl": "./JS/Engagement_checksum.json"}
 
